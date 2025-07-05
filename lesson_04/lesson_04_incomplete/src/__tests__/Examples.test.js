b79eb24fcf421f7d8d994ac694c712e0eecbf839
Mat Ruschel <mruschel@tribalscale.com>
2025-07-05 17:27:18 -0400

    [MR] chore: Added example test to lesson 4 incomplete

---
+++ lesson_04/lesson_04_incomplete/src/__tests__/Examples.test.js
@@ -0,0 +1,5 @@
+
+// Test Suite ->  A set of tests for a unit (nUnit calls this a testFixture)
+// Spec (or test) -> a single test within a suite.  (it or test)
+// Matchers -> methods that let you test objects in various ways.  (.toBe(4) is the matcher)
+// Assertions -> a spec is made up of assertions, that produce true or false values using matchers.
