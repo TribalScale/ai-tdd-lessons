3dbd896b5241958b4520cfee73099fd8fc267d44
Mat Ruschel <mruschel@tribalscale.com>
2025-07-08 21:55:10 -0400

    [MR] chore: updated tests in lesson 04 incomplete

---
+++ lesson_04/lesson_04_incomplete/src/__tests__/App.snapshot.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { App } from '../App';
+import { render, screen } from '@testing-library/react';
+
+describe('App snapshot', () => {
+    it('matches the snapshot', () => {
+
+    })
+})
